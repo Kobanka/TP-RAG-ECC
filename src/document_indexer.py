425f840e507e33a1e7f271786c35eac4abcecc93
laraisse <laraisse66@gmail.com>
2025-11-16 15:25:36 +0100

    optimisation 1
    Refactor Indexation class to optimize loading and embedding processes.

--- src/document_indexer.py
+++ src/document_indexer.py
@@ -3,40 +3,43 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_chroma import Chroma
 
-class Indexation :
-    def __init__(self,path,chunk_size,chunk_overlap):
+class Indexation:
+    def __init__(self, path, chunk_size, chunk_overlap):
         self.path = path
         self.chunk_size = chunk_size
         self.chunk_overlap = chunk_overlap
 
+        self.documents = None
+        self.chunks = None
+        self.embedding_model = None
+        self.vectorstore = None
+
     def data_load(self):
         loader = PyPDFLoader(self.path)
-        return loader.load()
-        
+        self.documents = loader.load()
+        return self.documents
     def splitting(self):
         text_splitter = RecursiveCharacterTextSplitter(
             chunk_size=self.chunk_size,
             chunk_overlap=self.chunk_overlap,
             add_start_index=True,
         )
-        all_splits = text_splitter.split_documents(self.data_load())
+        all_splits = text_splitter.split_documents(self.documents)
+        self.chunks = all_splits
         return all_splits
 
     def embedding(self):
-        #j'ai utilisé le modele huggingFace recommander dans la documentation de langchain
-        embedding_model = HuggingFaceEmbeddings(model_name="sentence-transformers/all-mpnet-base-v2")  
-        return embedding_model
+        if self.embedding_model is None:
+            self.embedding_model = HuggingFaceEmbeddings(model_name="sentence-transformers/all-mpnet-base-v2")
+        return self.embedding_model
 
     def vector_store(self):
-        vector_store = Chroma.from_documents(
-            documents=self.splitting(),
-            embedding=self.embedding(),
-            persist_directory="./chroma_langchain_db"
-        )
-        return vector_store
-
-
-
-
-
+        # optimisation du modele comme ca on a pas besoin de tous recalculer si c'est deja remplie
+        if self.vectorstore is None:  
+            self.vectorstore = Chroma.from_documents(
+                documents=self.chunks,
+                embedding=self.embedding(),
+                persist_directory="./chroma_langchain_db"
+            )
+        return self.vectorstore
 
