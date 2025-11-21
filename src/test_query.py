e62c9a48201b2402edf01791a301b25a0f900c47
imantachrift <140084705+Imantsh1@users.noreply.github.com>
2025-11-21 16:50:34 +0100

    Create test_query.py

---
+++ src/test_query.py
@@ -0,0 +1,13 @@
+from src.retriever import Recherche
+
+retr = Recherche()
+
+query = "Quelles sont les principales idées du document ?"
+
+results = retr.query(query, k=5)
+
+for r in results:
+    print("----")
+    print("Document :", r['document'])
+    print("Score :", r['score'])
+    print("Snippet :", r['content'])
