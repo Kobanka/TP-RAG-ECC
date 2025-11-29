ec64365cd078d3aa01ad8c38fcf0f512b7f6f399
laraisse <laraisse66@gmail.com>
2025-11-29 20:04:50 +0100

    Load environment variables from specific file
    load_dotenv() cherche le .env seulement dans src hors comme .env est hors src j'ai spécifié son path

--- src/test_qa.py
+++ src/test_qa.py
@@ -4,8 +4,10 @@ from src.rag_qa import RAGQuestionAnswering
 from langchain_openai import ChatOpenAI
 import os
 from dotenv import load_dotenv
-load_dotenv()
+
 #charger la clé API
+dotenv_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), "file.env")
+load_dotenv(dotenv_path)
 openrouter_api_key = os.getenv("OPENROUTER_API_KEY")
 
 llm = ChatOpenAI(
